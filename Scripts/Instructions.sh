e4d52db3495c5bdd034083a19bd0da36b38ddf75
Edward Hutter <hutter2@illinois.edu>
2019-07-26 17:58:54 -0500

    Many more additions to instructions. Issue #20

--- Scripts/Instructions.sh
+++ Scripts/Instructions.sh
@@ -7,7 +7,23 @@ LibraryPaths=("camfs" "candmc")
 
 # ********************************************************************************************************************************
 # BinaryPath - where binaries (of all libraries) are stored before transfer to SCRATCH
-BinaryPath=${LibraryPath}/src/bin
+if [ "$(hostname |grep "porter")" != "" ];
+then
+  camfsDir=~/hutter2/CAMFS
+elif [ "$(hostname |grep "mira")" != "" ] || [ "$(hostname |grep "cetus")" != "" ];
+then
+  camfsDir=~/scratch/CAMFS
+elif [ "$(hostname |grep "theta")" != "" ];
+then
+  camfsDir=~/scratch/CAMFS
+elif [ "$(hostname |grep "stampede2")" != "" ];
+then
+  camfsDir=~/CAMFS
+elif [ "$(hostname |grep "h2o")" != "" ];
+then
+  camfsDir=~/CAMFS
+fi
+BinaryPath=${camfsDir}/src/bin
 
 # ********************************************************************************************************************************
 # Algorithm tags (5 predefined)
@@ -53,3 +69,41 @@ analyzeDecision1=1
 # ********************************************************************************************************************************
 # analyzeDecision2 - profile using TAU
 analyzeDecision2=0
+
+# ********************************************************************************************************************************
+# mpiType - specify 'mpi' (unless on Porter, then 'ampi' is available)
+mpiType=mpi
+
+# ********************************************************************************************************************************
+# minPEcountPerNode/maxPEcountPerNodempiType - specify the min and max number of ppn x threads
+minPEcountPerNode=""
+maxPEcountPerNode=""
+if [ "$(hostname |grep "porter")" != "" ];
+then
+  machineName=PORTER
+  if [ "${mpiType}" == "mpi" ];
+  then
+    minPEcountPerNode=1
+    maxPEcountPerNode=16
+  elif [ "${mpiType}" == "ampi" ];
+  then
+    minPEcountPerNode=1
+    maxPEcountPerNode=512
+  fi
+elif [ "$(hostname |grep "mira")" != "" ] || [ "$(hostname |grep "cetus")" != "" ];
+then
+  minPEcountPerNode=16		# Note: this will need to be changed before launching Critter runs
+  maxPEcountPerNode=32
+elif [ "$(hostname |grep "theta")" != "" ];
+then
+  minPEcountPerNode=64		# Note: this will need to be changed before launching Critter runs
+  maxPEcountPerNode=128
+elif [ "$(hostname |grep "stampede2")" != "" ];
+then
+  minPEcountPerNode=64		# Note: this will need to be changed before launching Critter runs
+  maxPEcountPerNode=128
+elif [ "$(hostname |grep "h2o")" != "" ];
+then
+  minPEcountPerNode=16
+  maxPEcountPerNode=32
+fi
