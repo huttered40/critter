8c906f846a6e329a0253b12a1307b20a484ef944
Edward Hutter <hutter2@illinois.edu>
2019-08-08 16:18:27 -0500

    Trackign setup.py

---
+++ setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='critter',
+      author='Edward Hutter,Edgar Solomonik',
+      author_email='hutter2@illinois.edu,solomon2@illinois.edu',
+      packages=['critter'],
+      install_requires=[''])
