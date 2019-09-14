22f1bf410778225bd7a0f3eb08feeb542b4150e3
Edward Hutter <hutter2@illinois.edu>
2019-09-14 16:21:35 -0500

    Added back the mods to the recent SS QR test.

--- experiments/instructions_ST_SS_camfs_cqr2.py
+++ experiments/instructions_ST_SS_camfs_cqr2.py
@@ -11,11 +11,11 @@ from libraries import (camfs)
 CritterPath=os.environ["HOME"]+"/critter/"
 MachineType=stampede2
 LibraryTypeList=[camfs]
-fileID="benchQR1"
+fileID="commcost_vs_ppn"
 roundID=1
 NumLaunchesPerBinary=1
 numTests=1
-numHours="01"
+numHours="04"
 numMinutes="00"
 numSeconds="00"
 email="hutter2@illinois.edu"
@@ -24,25 +24,25 @@ if (os.system("hostname |grep \"porter\"") != 256):
     maxPEcountPerNode=16
 elif (os.system("hostname |grep \"stampede2\"") != 256):
     minPEcountPerNode=64          # Note: this will need to be changed before launching Critter runs
-    maxPEcountPerNode=128
+    maxPEcountPerNode=64
 elif (os.system("hostname |grep \"h2o\"") != 256):
     minPEcountPerNode=16
     maxPEcountPerNode=32
-nodeMinList=[1]
-nodeMaxList=[8]
-ppnMinList=[32]
+nodeMinList=[32]
+nodeMaxList=[256]
+ppnMinList=[1]
 ppnMaxList=[64]
 tprMinList=[1]
-tprMaxList=[4]
+tprMaxList=[64]
 nodeScaleFactorList=[2]
-ppnScaleFactorList=[2]
-tprScaleFactorList=[2]
+ppnScaleFactorList=[8]
+tprScaleFactorList=[8]
 nodeScaleOperatorList=[__mul__]
 ppnScaleOperatorList=[__mul__]
 tprScaleOperatorList=[__mul__]
 Algorithm1 = algorithm("camfs_cacqr2",\
-                       [8192,512,1,0,0,0,3],\
-		       [8192,512,8,0,0,0,3],\
+                       [65536,4096,2,0,0,0,3],\
+		       [65536,4096,16,0,0,0,3],\
 		       [1,1,2,1,1,1,1],\
 		       [__mul__,__mul__,__mul__,__mul__,__mul__,__mul__,__mul__],\
                        lambda x: 0,\
@@ -50,13 +50,13 @@ Algorithm1 = algorithm("camfs_cacqr2",\
 		       [[1,1,1,1,1,1,1]],\
 		       [[__mul__,__mul__,__mul__,__mul__,__mul__,__mul__,__mul__]])
 File1 = [["critter",[]],["perf",["Performance","Residual","Deviation from Orthogonality"]]]
-Test1=[[Algorithm1],"Strong Scaling: 8192x512 matrix",File1]
+Test1=[[Algorithm1],"Strong Scaling: 65536x4096 matrix",File1]
 TestList=[Test1]
 
 Launcher = bench(CritterPath,MachineType,LibraryTypeList,fileID,roundID,NumLaunchesPerBinary,\
                  numTests,numHours,numMinutes,numSeconds,email,minPEcountPerNode,maxPEcountPerNode,\
 		 nodeMinList,nodeMaxList,ppnMinList,ppnMaxList,tprMinList,tprMaxList,nodeScaleFactorList,ppnScaleFactorList,tprScaleFactorList,\
                  nodeScaleOperatorList,ppnScaleOperatorList,tprScaleOperatorList,TestList)
-#Launcher.build()
+Launcher.build()
 Launcher.generate()
-#Launcher.launch()
+Launcher.launch()
