ee315912af29c4014021218c0e6defabdd0b3334
Edward Hutter <hutter2@illinois.edu>
2019-10-17 13:55:16 -0500

    More modifications to benchmark files. Issue #5

--- bmark/test_reduce.cxx
+++ bmark/test_reduce.cxx
@@ -2,28 +2,26 @@
 #include <random>
 
 int main(int argc, char ** argv){
+  size_t msg_size = atoi(argv[1]);
+  size_t sub_comm_size = atoi(argv[2]);
   MPI_Init(&argc, &argv);
   int rank, p;
   MPI_Comm_rank(MPI_COMM_WORLD, &rank);
   MPI_Comm_size(MPI_COMM_WORLD, &p);
   srand48(rank);
-  for (int msg_size = 1; msg_size<=(1<<20); msg_size<<=1){
-    double * buf = (double*)malloc(msg_size*sizeof(double));
-    for (int j=0; j<msg_size; j++){
-      buf[j] = drand48();
-    }
-    auto pcount=1;
-    while (pcount<p){
-      MPI_Comm sub_comm;
-      MPI_Comm_split(MPI_COMM_WORLD, rank<pcount, rank, &sub_comm);
-      critter::start();
-      MPI_Reduce(MPI_IN_PLACE, buf, msg_size, MPI_DOUBLE, MPI_SUM, pcount-1, sub_comm);
-      critter::stop();
-      MPI_Comm_free(&sub_comm);
-      pcount*=2;
-    }
-    free(buf);
+  double* buf = (double*)malloc(msg_size*sizeof(double));
+  /*for (int j=0; j<msg_size; j++){
+    buf[j] = drand48();
+  }*/
+  MPI_Comm sub_comm;
+  MPI_Comm_split(MPI_COMM_WORLD, rank<sub_comm_size, rank, &sub_comm);
+  for (auto i=0; i<3; i++){
+    critter::start();
+    MPI_Reduce(MPI_IN_PLACE, buf, msg_size, MPI_DOUBLE, MPI_SUM, sub_comm_size-1, sub_comm);
+    critter::stop();
   }
+  MPI_Comm_free(&sub_comm);
+  free(buf);
   MPI_Finalize();
   return 0;
 }
--- bmark/test_sendrecv_replace.cxx
+++ bmark/test_sendrecv_replace.cxx
@@ -2,35 +2,33 @@
 #include <random>
 
 int main(int argc, char ** argv){
+  size_t msg_size = atoi(argv[1]);
+  size_t sub_comm_size = atoi(argv[2]);
   MPI_Init(&argc, &argv);
   int rank, p;
   MPI_Comm_rank(MPI_COMM_WORLD, &rank);
   MPI_Comm_size(MPI_COMM_WORLD, &p);
   srand48(rank);
-  for (int msg_size = 1; msg_size<=(1<<20); msg_size<<=1){
-    double * buf = (double*)malloc(msg_size*sizeof(double));
-    for (int j=0; j<msg_size; j++){
-      buf[j] = drand48();
-    }
-    auto pcount=1;
-    while (pcount<p){
-      MPI_Comm sub_comm;
-      MPI_Comm_split(MPI_COMM_WORLD, rank<pcount, rank, &sub_comm);
-      MPI_Status st;
-      int partner=0;
-      if (rank < (pcount>>1)){
-        partner+=(pcount>>1);
-      } else{
-        partner-=(pcount>>1);
-      }
-      critter::start();
-      MPI_Sendrecv_replace(buf, msg_size, MPI_DOUBLE, partner, 0, partner, 0, sub_comm, &st);
-      critter::stop();
-      MPI_Comm_free(&sub_comm);
-      pcount*=2;
-    }
-    free(buf);
+  double* buf = (double*)malloc(msg_size*sizeof(double));
+  /*for (int j=0; j<msg_size; j++){
+    buf[j] = drand48();
+  }*/
+  MPI_Comm sub_comm;
+  MPI_Comm_split(MPI_COMM_WORLD, rank<sub_comm_size, rank, &sub_comm);
+  MPI_Status st;
+  int partner=0;
+  if (rank < (sub_comm_size>>1)){
+    partner+=(sub_comm_size>>1);
+  } else{
+    partner-=(sub_comm_size>>1);
   }
+  for (auto i=0; i<3; i++){
+    critter::start();
+    MPI_Sendrecv_replace(buf, msg_size, MPI_DOUBLE, partner, 0, partner, 0, sub_comm, &st);
+    critter::stop();
+  }
+  MPI_Comm_free(&sub_comm);
+  free(buf);
   MPI_Finalize();
   return 0;
 }
